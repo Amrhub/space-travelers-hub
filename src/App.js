30c00c67b5b2a5510f959320751e131000030e00
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-12-13 14:54:09 -0500

    feat: Add my profile page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Profile from './components/Profile';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Router>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
