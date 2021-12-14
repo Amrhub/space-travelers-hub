c760bd2d4d026dcb8cd62df20a63ee190777b232
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-12-14 12:50:06 -0500

    feat: Filter data from API

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -19,8 +19,13 @@ export const populateMissions = (payload) => ({
 export const fetchMissions = () => async (dispatch) => {
   const response = await fetch('https://api.spacexdata.com/v3/missions');
   const missions = await response.json();
+  const newMissions = missions.map((mission) => ({
+    mission_id: mission.mission_id,
+    mission_name: mission.mission_name,
+    description: mission.description,
+  }));
   if (response.ok) {
-    dispatch(populateMissions(missions));
+    dispatch(populateMissions(newMissions));
   }
 };
 
