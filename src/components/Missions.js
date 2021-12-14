e3f2b3f50fb33c7d27ca585b31012b933ef653c9
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-12-14 12:50:40 -0500

    feat: Display mission table

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,22 +1,41 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchMissions } from '../redux/missions/missions';
+import './Missions.scss';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missions);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchMissions());
+    if (!missions.length) dispatch(fetchMissions());
   }, []);
 
   return (
-    <main>
-      <ul>
-        {missions.map((mission) => (
-          <li key={mission.mission_id}>{mission.mission_name}</li>
-        ))}
-      </ul>
+    <main className="main">
+      <table className="missions-table">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th className="status-col">Status</th>
+            <th className="join-col">Join</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission, index) => {
+            const colorClass = index % 2 === 0 ? 'grey-bg' : 'white-bg';
+            return (
+              <tr key={mission.mission_id} className={colorClass}>
+                <th className="mission-title">{mission.mission_name}</th>
+                <td>{mission.description}</td>
+                <td className="mission-status">Not a member</td>
+                <td className="mission-join">Join Mission</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </main>
   );
 };
