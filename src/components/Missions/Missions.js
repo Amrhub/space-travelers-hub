8775586564e8ff0bd4e64e27e654ab9365894f5b
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-12-14 15:14:03 -0500

    Fix: Replace th for td

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -27,7 +27,7 @@ const Missions = () => {
             const colorClass = index % 2 === 0 ? 'grey-bg' : 'white-bg';
             return (
               <tr key={mission.mission_id} className={colorClass}>
-                <th className="mission-title">{mission.mission_name}</th>
+                <td className="mission-title">{mission.mission_name}</td>
                 <td>{mission.description}</td>
                 <td className="mission-status">Not a member</td>
                 <td className="mission-join">Join Mission</td>
