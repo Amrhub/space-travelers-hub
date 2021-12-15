41fcf9baed9eac9a47c4aedb39a5aef60fd09781
Amrhub <amr_ahmed65@hotmail.com>
2021-12-15 20:47:01 +0200

    feat: basic structure for Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import ReservedRockets from './ReservedRockets';
+import './Profile.scss';
 
-const Profile = () => <main className="main">Under construction</main>;
+const Profile = () => (
+  <main className="main profile">
+    <ReservedRockets />
+    {/* for missions */}
+    <section />
+  </main>
+);
 
 export default Profile;
---
+++ src/components/Profile/ReservedRockets.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ReservedRockets = () => <section className="reserved-rockets" />;
+
+export default ReservedRockets;
