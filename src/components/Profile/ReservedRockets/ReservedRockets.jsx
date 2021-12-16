f45112e2cc288792d8f88866c840b1f0e60a6b3d
Amrhub <amr_ahmed65@hotmail.com>
2021-12-16 03:27:12 +0200

    feat: generalize filtered list style

--- src/components/Profile/ReservedRockets/ReservedRockets.jsx
+++ src/components/Profile/ReservedRockets/ReservedRockets.jsx
@@ -1,24 +1,31 @@
+/* eslint-disable operator-linebreak */
 import _ from 'lodash';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import './ReservedRockets.scss';
+import '../filteredList.scss';
 
 const ReservedRockets = () => {
   const rockets = useSelector((state) => state.rockets);
   const reservedRockets = _.filter(rockets, (rocket) => rocket.isReserved);
 
   return (
-    <section className="reserved-rockets">
-      <h2 className="reserved-rockets__title">My Rockets</h2>
-      <ul className="reserved-rockets__list">
-        {_.map(reservedRockets, (rocket) => (
-          <li className="reserved-rockets__list__item" key={rocket.id}>
-            <h3 className="reserved-rockets__list__item__title">
-              {rocket.name}
-            </h3>
-          </li>
-        ))}
+    <section className="filtered-list">
+      <h2 className="filtered-list__title">My Rockets</h2>
+      <ul className="filtered-list__list">
+        {!!reservedRockets &&
+          _.map(reservedRockets, (rocket) => (
+            <li className="filtered-list__list__item" key={rocket.id}>
+              <h3 className="filtered-list__list__item__title">
+                {rocket.name}
+              </h3>
+            </li>
+          ))}
+        {!reservedRockets.length && (
+          <p className="filtered-list__list__item">
+            There are no reserved rockets
+          </p>
+        )}
       </ul>
     </section>
   );
