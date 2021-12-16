07b92ed26089805df2f6e157162d0a38f3f18c5b
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-12-16 14:37:20 -0500

    feat: Include buttons on profile mission list

--- src/components/Profile/ReservedMissions/ReservedMissions.jsx
+++ src/components/Profile/ReservedMissions/ReservedMissions.jsx
@@ -1,22 +1,34 @@
-/* eslint-disable operator-linebreak */
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import Button from '../../../abstracts/styledComponents/Button.style';
+import Link from '../../../abstracts/styledComponents/Link.style';
+import { toggleMissionReservation } from '../../../redux/missions/missions';
 import '../filteredList.scss';
 
 const ReservedMissions = () => {
   const missions = useSelector((state) => state.missions);
   const reservedMissions = missions.filter((mission) => mission.reserved);
+  const dispatch = useDispatch();
+
+  const handleMissionReservation = (id) => {
+    dispatch(toggleMissionReservation(id));
+  };
 
   return (
     <section className="filtered-list">
       <h2 className="filtered-list__title">My Missions</h2>
       <ul className="filtered-list__list">
-        {!!reservedMissions &&
-          reservedMissions.map((mission) => (
+        {!!reservedMissions
+          && reservedMissions.map((mission) => (
             <li className="filtered-list__list__item" key={mission.mission_id}>
               <h3 className="filtered-list__list__item__title">
                 {mission.mission_name}
               </h3>
+              <Link to={mission.link} />
+              <Button
+                isMissionJoined={mission.reserved}
+                onClick={() => handleMissionReservation(mission.mission_id)}
+              />
             </li>
           ))}
         {!reservedMissions.length && (
