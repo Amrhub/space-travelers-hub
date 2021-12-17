6048fbd8040eea34c30ea7e665d842dec708292b
Amrhub <amr_ahmed65@hotmail.com>
2021-12-17 20:30:36 +0200

    fix: remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import store from './redux/configureStore';
 
 ReactDOM.render(
@@ -17,8 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
